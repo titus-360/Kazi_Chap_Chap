df989f882b9756fd435bbfd72bb8bea2f8653d04
titus-360 <murithititus510@gmail.com>
2024-10-26 11:56:51 +0300

    initial commit

---
+++ src/main/kotlin/com/job_portal/jobportal/JobPortalApplication.kt
@@ -0,0 +1,11 @@
+package com.job_portal.jobportal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JobPortalApplication
+
+fun main(args: Array<String>) {
+    runApplication<JobPortalApplication>(*args)
+}
---
+++ src/test/kotlin/com/job_portal/jobportal/JobPortalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.job_portal.jobportal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
